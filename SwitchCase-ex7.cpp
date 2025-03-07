0f3805f3d71d2d4bddd8ccbed80916143ff0929d
Tannishtha Saha <tsaha@terpmail.umd.edu>
2025-03-07 20:23:33 +0000

    EX7

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,27 @@
-// add your code here
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    // Prompt user for input
+    printf("Please enter an integer value: ");
+    scanf("%d", &num);
+    
+    // Switch case logic
+    switch (num) {
+        case 0: case 2: case 4: case 6: case 8:
+            printf("%d squared is %d\n", num, num * num);
+            break;
+        default:
+            if (num % 2 != 0) {
+                printf("%d divided by 2 is %.1f\n", num, num / 2.0);
+            } else {
+                printf("hahaha\n");
+            }
+            break;
+    }
+    
+    return 0; // Indicate successful program execution
+}
+
+
