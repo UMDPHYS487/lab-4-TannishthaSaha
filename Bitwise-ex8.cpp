4b68cbd9406abd23b0f735df3a973b1523fd5f54
Tannishtha Saha <tsaha@terpmail.umd.edu>
2025-03-07 20:28:22 +0000

    EX8

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,13 @@
-// Your code here
+#include <iostream>
+#include <bitset>
+
+int main() {
+    std::bitset<8> b{0b01110010}; // Define a bitset with an initial value
+    std::cout << b << " (initial value)\n";
+
+    b = ~b; // Flip all bits using the bitwise NOT operator
+
+    std::cout << b << " (flipped value)\n"; // Print the modified bitset
+
+    return 0;
+}
