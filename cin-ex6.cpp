201559c739fc670113f263bf579d11eab733bf05
Tannishtha Saha <tsaha@terpmail.umd.edu>
2025-03-07 20:14:42 +0000

    EX6

---
+++ cin-ex6.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int y = 10; // Define an integer outside the loop
+    int *x = &y; // Define a pointer and set it to point to y
+    
+    // Print memory address of y
+    printf("Memory address of y: %p\n", (void*)&y);
+    printf("Memory address of y: %p\n", (void*)&y);
+    printf("Memory address of y: %p\n", (void*)&y);
+    printf("\n");
+    
+    // Print value of y and value stored at pointer x
+    printf("Initial value of y: %d\n", y);
+    printf("Value at pointer x: %d\n", *x);
+    printf("\n");
+    
+    // Change the value of y
+    y = 20;
+    
+    // Print value of y and value stored at pointer x after modification
+    printf("Modified value of y: %d\n", y);
+    printf("Value at pointer x after modifying y: %d\n", *x);
+    
+}
+
